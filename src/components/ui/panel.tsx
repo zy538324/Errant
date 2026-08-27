707a4e162e2453cd50c439c67435f5eb6cac85e0
zy538324 <81492324+zy538324@users.noreply.github.com>
2026-08-27 19:13:38 +0000

    Finalize cleanup of legacy source files for full Laravel PHP conversion

--- src/components/ui/panel.tsx
+++
@@ -1,6 +0,0 @@
-import * as React from "react";
-import { cn } from "@/lib/utils";
-
-export function Panel({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
-  return <div className={cn("panel-surface rounded-[2rem]", className)} {...props} />;
-}
